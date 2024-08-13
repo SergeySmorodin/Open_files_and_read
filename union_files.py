ad14174dbeef494661f59b259bfb839b510d47f5
SergeySmorodin <ssmorodin@bk.ru>
2024-08-13 12:43:58 +0300

    Update union_files.py

--- union_files.py
+++ union_files.py
@@ -18,3 +18,16 @@ with open('result.txt', 'w', encoding='utf-8') as result_file:
         result_file.writelines(lines)
 
 print("Объединение завершено. Результат записан в 'result.txt'.")
+
+
+# Вариант 2
+def merge_files(file_names):
+    sorted_file_data = sorted(((name, len(open(name, encoding='utf-8').readlines())) for name in file_names), key=lambda x: x[1])
+
+    with open('result.txt', 'w', encoding='utf-8') as result_file:
+        for file_name, line_count in sorted_file_data:
+            file_content = open(file_name, encoding='utf-8').read()
+            result_file.write(f'Имя файла: {file_name}\\n')
+            result_file.write(f'Количество строк: {line_count}\\n')
+            result_file.write(file_content + '\\n\\n')
+
